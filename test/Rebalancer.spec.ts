18d8e2421bb0c279379adcbd67c25b471d0c0bb6
havi-kim <hyochankim94@gmail.com>
2022-10-28 05:27:48 +0900

    test: Add rebalancer detail test code

--- test/Rebalancer.spec.ts
+++ test/Rebalancer.spec.ts
@@ -8,6 +8,7 @@ import {
     UniswapV3AutoRebalancer
 } from '../typechain'
 import rebalancerFixture from './shared/rebalancerFixture'
+import {expect} from "chai";
 
 describe('UniswapV3AutoRebalancer', () => {
     let factory: IUniswapV3Factory
@@ -26,24 +27,27 @@ describe('UniswapV3AutoRebalancer', () => {
         await usdc.approve(rebalancer.address, constants.MaxUint256)
     })
 
-    describe('#success test', async () => {
+    describe('#simple success test', async () => {
         it('deposit success test 1', async () => {
-            await rebalancer.deposit("1000000000000000000", "1");
+            await rebalancer.deposit("1000000", "1");
         })
         it('deposit success test 2', async () => {
-            await rebalancer.deposit("2000000000000000000", "3");
+            await rebalancer.deposit("20000000", "2");
         })
         it('deposit success test 3 ', async () => {
-            await rebalancer.deposit("300000000000000000", "5");
+            await rebalancer.deposit("300000000", "3");
         })
         it('deposit success test 4 ', async () => {
-            await rebalancer.deposit("5000000000000000000", "3");
+            await rebalancer.deposit("4000000000", "3");
         })
         it('deposit success test 5 ', async () => {
-            await rebalancer.deposit("10000000000000000000", "4");
+            await rebalancer.deposit("100000000000", "1");
         })
         it('deposit success test 6 ', async () => {
-            await rebalancer.deposit("10000000000000000000", "9");
+            await rebalancer.deposit("100000000000", "3");
+        })
+        it('deposit success test 7 ', async () => {
+            await rebalancer.deposit("100000000000", "3");
         })
 
         it('withdraw success test 1', async () => {
@@ -57,13 +61,93 @@ describe('UniswapV3AutoRebalancer', () => {
         })
 
         it('triggerRebalance success test 1 ', async () => {
-            await rebalancer.triggerRebalance("4", "3");
+            await rebalancer.triggerRebalance("4", "1");
         })
         it('triggerRebalance success test 2 ', async () => {
-            await rebalancer.triggerRebalance("5", "3");
+            await rebalancer.triggerRebalance("5", "2");
         })
         it('triggerRebalance success test 3 ', async () => {
             await rebalancer.triggerRebalance("6", "3");
+        })
+    })
+
+    describe('#detailed test', async () => {
+        it('open position test', async () => {
+            const [tester] = await ethers.getSigners();
+            let inputUSDC = "10000000000";
+            let positionId = await rebalancer.nextPositionId();
+            await rebalancer.deposit(inputUSDC, "1");
+            let position = await rebalancer.positions(positionId);
+            expect(position.owner).to.eq(tester.address, "Position owner should tester");
+            expect(position.liquidity.toString()).to.not.eq("0", "Position liquidity should not 0");
+            expect(position.tickLower.toString()).to.not.eq("0", "Position tickLower should not 0");
+            expect(position.tickUpper.toString()).to.not.eq("0", "Position tickUpper should not 0");
+            expect(position.feeGrowthInside0LastX128.toString()).to.not.eq(
+                "0", "Position feeGrowthInside0LastX128 should not 0"
+            );
+            expect(position.feeGrowthInside1LastX128.toString()).to.not.eq(
+                "0", "Position feeGrowthInside1LastX128 should not 0"
+            );
+        })
+
+        it('close position test', async () => {
+            let positionId = await rebalancer.nextPositionId();
+            await rebalancer.withdraw(positionId.sub(1));
+            let position = await rebalancer.positions(positionId.sub(1));
+            expect(position.owner).to.eq("0x0000000000000000000000000000000000000000",
+                "Position owner should blank"
+            );
+            expect(position.liquidity.toString()).to.eq("0", "Position liquidity should 0");
+            expect(position.tickLower.toString()).to.eq("0", "Position tickLower should 0");
+            expect(position.tickUpper.toString()).to.eq("0", "Position tickUpper should 0");
+            expect(position.feeGrowthInside0LastX128.toString()).to.eq(
+                "0", "Position feeGrowthInside0LastX128 should 0"
+            );
+            expect(position.feeGrowthInside1LastX128.toString()).to.eq(
+                "0", "Position feeGrowthInside1LastX128 should 0"
+            );
+        })
+
+        it('USDC deposit test', async () => {
+            const [tester] = await ethers.getSigners();
+            let inputUSDC = "10000000000";
+            let beforeUSDCBalance = await usdc.balanceOf(tester.address);
+            await rebalancer.deposit(inputUSDC, "1");
+            let afterUSDCBalance = await usdc.balanceOf(tester.address);
+            expect(beforeUSDCBalance.sub(afterUSDCBalance).toString()).to.eq(
+                inputUSDC, "Should same amount"
+            );
+        })
+
+        it('USDC withdraw test', async () => {
+            const [tester] = await ethers.getSigners();
+            let beforeUSDCBalance = await usdc.balanceOf(tester.address);
+            let positionId = await rebalancer.nextPositionId();
+            await rebalancer.withdraw(positionId.sub(1));
+            let afterUSDCBalance = await usdc.balanceOf(tester.address);
+            expect(beforeUSDCBalance.toString()).to.not.eq(
+                afterUSDCBalance.toString(), "Should not same amount"
+            );
+        })
+
+        it('Rebalance test', async () => {
+            let originalPosition = await rebalancer.positions("7");
+            await rebalancer.triggerRebalance("7", "1");
+            let rebalancedPosition = await rebalancer.positions("7");
+            expect(originalPosition.owner).to.eq(rebalancedPosition.owner, "Position owner should same");
+            expect(originalPosition.liquidity).to.not.eq(rebalancedPosition.liquidity,
+                "Position liquidity should not same"
+            );
+        })
+
+        it('USDC Rebalance test', async () => {
+            const [tester] = await ethers.getSigners();
+            let beforeUSDCBalance = await usdc.balanceOf(tester.address);
+            await rebalancer.triggerRebalance("7", "1");
+            let afterUSDCBalance = await usdc.balanceOf(tester.address);
+            expect(beforeUSDCBalance.toString()).to.eq(
+                afterUSDCBalance.toString(), "Should same amount"
+            );
         })
     })
 })
